699daf04014391227a56d6ca15520114a31a11ed
unknown <chez90@mail.ru>
2019-09-20 20:41:20 +0400

    Create activeQuiz, formate it

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ActiveQuiz.css'
+
+const ActiveQuiz = props => (
+	<div className="ActiveQuiz">
+		<p className='Question'>
+			<span>
+				<strong>2.</strong>&nbsp;
+				Как дела?
+			</span>
+			<small>3 из 15</small>
+		</p>
+
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+)
+
+export default ActiveQuiz
