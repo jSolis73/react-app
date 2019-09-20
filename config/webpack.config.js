888c1d629a477a81ffd8ce5da37f37eabc05bf5e
unknown <chez90@mail.ru>
2019-09-20 18:24:05 +0400

    add main page, css. Add one quiz

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -86,6 +86,8 @@ module.exports = function(webpackEnv) {
       {
         loader: require.resolve('css-loader'),
         options: cssOptions,
+          // modules: true,
+          // localIdentName: '[name]__[local]--[hash:base64:5]'
       },
       {
         // Options for PostCSS as we reference these options twice
